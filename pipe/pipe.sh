5eb0f314dd611792312acd66a37e41a05b50cfc4
Matt Miller <mmiller@grafik.com>
2021-12-09 22:48:35 -0500

    updated curl string

--- pipe/pipe.sh
+++ pipe/pipe.sh
@@ -24,7 +24,7 @@ ARTIFACT=${ARTIFACT:?'ARTIFACT variable missing.'}
 
 backup_wpe_install() {
     info "Backing up site before git push..."
-        curl -X POST "https://api.wpengineapi.com/v1/installs/${WPE_INSTALL_ID}/backups" -H "Content-Type: application/json" -d {"description": "Before deploy", "notification_emails": ["mmiller@grafik.com"]} -u ${WPE_API_USER}:${WPE_API_PASSWORD}
+        curl -X POST "https://api.wpengineapi.com/v1/installs/${WPE_INSTALL_ID}/backups" -H "Content-Type: application/json" -d '{"description": "Before deploy", "notification_emails": ["mmiller@grafik.com"]}' -u ${WPE_API_USER}:${WPE_API_PASSWORD}
 }
 backup_wpe_install
 
